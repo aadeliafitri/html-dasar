6be0261fade244ec499fd16da083f3689356615a
aadeliafitri <adelia_fitri_27rpl@student.smktelkom-mlg.sch.id>
2023-09-13 21:11:35 +0700

    [FEAT] add landingdom.html

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -8,7 +8,7 @@ function plusSlides(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("slideshow");
-    console.log(slides.length)
+    // console.log(slides.length)
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
@@ -16,4 +16,19 @@ function showSlides(n) {
     }
 
     slides[slideIndex-1].style.display = "block";
+}
+
+let subTitle = document.getElementById("subtitle-slide");
+subTitle.innerText = "Don't miss it";
+subTitle.style.fontSize = "40px"
+setTimeout(() => {
+    subTitle.innerText = "Just Buy It"
+}, 3000);
+
+let titleAbout = document.getElementById("title-about");
+titleAbout.style.fontSize = "60px";
+
+
+function confirmOrder(params) {
+    confirm("Anda yakin ingin membeli kue ini?");
 }
